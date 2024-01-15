2b9b093188f22c80bce06b605fca1ea15b57ea8b
bmatheo <matheo.bayle@ynov.com>
2024-01-15 16:45:36 +0100

    fire

--- Assets/Scripts/Weapons/WeaponsController.cs
+++ Assets/Scripts/Weapons/WeaponsController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Weapon Stats")] 
+    public GameObject prefab;
+
+    public float damage;
+    public float speed;
+    public float cooldownDur;
+    private float currentCooldown;
+    public int pierce;
+    protected PlayerMovement playerMovement;
+    protected virtual void Start()
     {
-        
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        currentCooldown = cooldownDur;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown < 0f)
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        currentCooldown = cooldownDur;
     }
 }
